a7533e24aafee62ad9847796174d8e6837879c75
Kris <2218735@edtools.psd401.net>
2026-01-23 19:58:18 -0800

    brotli test i hope

--- DOOM/doom/compile.js
+++ DOOM/doom/compile.js
@@ -1,9 +1,21 @@
+window.TBtoZIP = async function(TB) {
+	let tar = brot.decompress(TB);
+	let decomp = await parseTar(tar);
+	let files = {};
+	for(let file of decomp) {
+		files[file.name] = file.data;
+	}
+	let zip = fflate.zipSync(files, { level: 1 });
+	return zip;
+}
+
 window.compileDoom = async function() {
   let parts = [];
   for(let i = 0; i < 12; i++) {
     let response = await fetch(`./doom/part${i}`);
     parts.push(Array.from(await response.bytes()));
   }
-  let blob = new Blob([new Uint8Array(parts.flat())], { type: 'application/octet-stream' });
+  let jsdos = await TBtoZIP(new Uint8Array(parts.flat()));
+  let blob = new Blob([jsdos], {type: "application/zip"});
   return URL.createObjectURL(blob);
 }
---
+++ DOOM/lib/brotli/brotli.js
@@ -0,0 +1,2 @@
+import init, * as brotli from "./brotli_wasm.js";
+export default init().then(() => brotli);
